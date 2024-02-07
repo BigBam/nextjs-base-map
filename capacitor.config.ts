aa3a160328b853919a17295da22fc07fcdc0c6a5
Bam Faboyede <bamwebsites@gmail.com>
2024-02-07 18:57:11 -0500

    Update configuration
    Set up static export

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'org.codemovers.basemapillary',
   appName: 'BaseMapillary',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     androidScheme: 'https'
   }
